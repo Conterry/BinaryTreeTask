42de20221c507835ae16b63ec5ee1367443bab33
Hleb Kriukov <glebos05@gmail.com>
2021-03-02 22:02:21 +0200

    Adds to tree

---
+++ BST/BST.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BST
+{
+    public class BST
+    {
+        private BST LeftElement;
+        private BST RightElement;
+        private int CurrentElement;
+
+        public BST(int FirstElementOfTree)
+        {
+            this.CurrentElement = FirstElementOfTree;
+        }
+
+        public void Add(int element)
+        {
+            if (element < this.CurrentElement) this.GoLeft(element);
+            if (element > this.CurrentElement) this.GoRight(element);
+
+        }
+
+        //private bool GoLeft(int element) => this.CurrentElement > element;
+        //private bool GoRight(int element) => this.CurrentElement < element;
+
+        private void GoLeft(int element)
+        {
+            if ((element < this.CurrentElement) && (this.LeftElement!=null))
+            {
+                this.LeftElement.GoLeft(element);
+            }
+            else if((element > this.CurrentElement) && (this.LeftElement != null))
+            {
+                this.LeftElement.GoRight(element);
+            }
+            else
+            {
+                this.LeftElement = new BST(element);
+            }
+        }
+
+        private void GoRight(int element)
+        {
+            if ((element < this.CurrentElement) && (this.RightElement!=null))
+            {
+                this.RightElement.GoLeft(element);
+            }
+            else if ((element > this.CurrentElement) && (this.RightElement != null))
+            {
+                this.RightElement.GoRight(element);
+            }
+            else
+            {
+                this.RightElement = new BST(element);
+            }
+        }
+
+        /*public int[] TreeElements()
+        {
+            List<int> Elements = new List<int>();
+            int previous;
+
+        }*/
+
+        public int FirstElement()
+        {
+            return this.LeftElement == null ? this.CurrentElement : this.LeftElement.FirstElement();
+        }
+
+    }
+}
---
+++ BST/BSTProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BST
+{
+    class BSTProgram
+    {
+        static void Main(string[] args)
+        {
+            BST tree = new BST(10);
+            tree.Add(12);
+            tree.Add(11);
+            tree.Add(17);
+            tree.Add(6);
+
+            Console.WriteLine(tree.FirstElement());
+        }
+    }
+}
